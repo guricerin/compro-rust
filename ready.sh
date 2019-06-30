b3e1cc9df788d98d1df05c4ad2613a10008008a7
guricerin <gurizumi@gmail.com>
2019-06-30 11:16:18 +0900

    a~d -> a~f

--- ready.sh
+++ ready.sh
@@ -16,14 +16,14 @@ fi
 
 mkdir -p ${bin_dir}
 
-for p_name in {a..d}; do
+for p_name in {a..f}; do
     cp ${temp_dir}/main.rs.temp ${bin_dir}/${p_name}.rs
 done
 
 # online-judge-tools が生成した問題毎のtestディレクトリをすべて削除。
 readonly target_dir="${base_path}/target/debug"
 
-for p_name in {a..d}; do
+for p_name in {a..f}; do
     test_dir="${target_dir}/${p_name}-test"
     rm -rf ${test_dir}
 done
