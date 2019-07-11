7f00fb1b8e53080319b5c16c94d15d4821399108
guricerin <gurizumi@gmail.com>
2019-07-11 13:08:56 +0900

    add warshall_floyd

--- src/graph/mod.rs
+++ src/graph/mod.rs
@@ -1,2 +1,3 @@
-pub mod graph;
 pub mod dijkstra;
+pub mod graph;
+pub mod warshall_floyd;
---
+++ src/graph/warshall_floyd.rs
@@ -0,0 +1,18 @@
+/// 隣接行列で表されるグラフの全点間最短路を求める。負辺があっても動作する。
+/// 負閉路が存在する場合はそれも検出する(ある頂点 k から k への最短路が負ならば負閉路が存在)。
+/// 到達できない要素には、infが格納される。
+/// O(V^3)
+fn warshall_floyd(matrix: &mut Vec<Vec<i64>>, inf: i64) {
+    let n = matrix.len();
+    assert_eq!(n, matrix[0].len());
+    for k in 0..n {
+        for i in 0..n {
+            for j in 0..n {
+                if matrix[i][k] == inf || matrix[k][j] == inf {
+                    continue;
+                }
+                matrix[i][j] = std::cmp::min(matrix[i][j], matrix[i][k] + matrix[k][j]);
+            }
+        }
+    }
+}
